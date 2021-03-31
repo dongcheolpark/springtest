950f06c1d67e8177225fb12b6d1e28a0e9a21a71
dongcheolpark <head02325@gmail.com>
2021-03-31 21:34:09 +0900

    layout

--- src/main/java/org/pdc/testspring/Controller/HelloController.java
+++ src/main/java/org/pdc/testspring/Controller/HelloController.java
@@ -1,2 +1,14 @@
-package org.pdc.testspring.Controller;public class HelloController {
+package org.pdc.testspring.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("Hello")
+    public String hello(Model model){
+        model.addAttribute("data","hello,world!");
+        return "/view/home/hello";
+    }
 }
